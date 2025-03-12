67bf898ee1a38c3a1dcc13cda2fa06f6b58f8ec2
Edson J Vargas <edson.jhair@yahoo.com>
2025-03-12 16:11:18 -0500

    Interfaces

---
+++ src/10 - interfaces/index.ts
@@ -0,0 +1,30 @@
+type Sizes = 'S' | 'M' | 'L' | 'XL';
+type userId = string | number;
+// type Product = {
+//   id: string | number;
+//   title: string;
+//   createdAt: Date;
+//   stock: number;
+//   size: Sizes;
+// };
+
+interface Product {
+  id: string | number;
+  title: string;
+  createdAt: Date;
+  stock: number;
+  size?: Sizes;
+}
+
+const products: Product[] = [];
+
+products.push({
+  id: '1',
+  title: '1',
+  createdAt: new Date(),
+  stock: 90,
+});
+
+const addProduct = (data: Product) => {
+  products.push(data);
+};
