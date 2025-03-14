53c6dcb77297236127f65334fd98449692a15211
Edson J Vargas <edson.jhair@yahoo.com>
2025-03-14 11:58:41 -0500

    Ejemplo CRUD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,29 @@
-import { addProduct } from './products/products.service';
+import { addProduct, products } from './products/products.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: '1',
-  title: '1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 90,
-  category: {
-    id: '12',
-    name: 'c1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-});
+for (let i = 0; i < 50; i++) {
+  addProduct({
+    id: faker.string.uuid(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({ min: 10, max: 100 }),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    category: {
+      id: faker.string.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    image: faker.image.url(),
+    description: faker.commerce.productDescription(),
+    color: faker.color.rgb(),
+    price: faker.commerce.price(),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.multiple(faker.commerce.productAdjective, {
+      count: { min: 1, max: 5 },
+    }),
+  });
+}
+
+console.log(products);
